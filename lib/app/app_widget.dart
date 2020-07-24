d6e180a4e6aac850cebd6b7a4fd9c72ee738b45f
Gyliarde Araujo <gyliardearaujo@Air-de-Gyliarde.meuintelbras.local>
2020-07-24 20:47:10 -0300

    first commit

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_auth_mobx/app/home_page.dart';
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:firebase_auth_mobx/app/app_widget.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
